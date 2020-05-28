87d70ca2414b67cec1ed94db6475522dccb0e581
James Rogers <james.rogers@spitewood.com>
2020-05-28 15:53:26 +0100

    Added sign out link to main nav

---
+++ src/Web/Dfe.CspdAlpha.Web/Dfe.CspdAlpha.Web.Application/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Dfe.CspdAlpha.Web.Application.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
