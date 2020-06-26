b1b5b2cbe1c675388c1aaa9e07e4ee278268bb62
Andy Rose <andy.rose@olivejar.co.uk>
2020-06-26 12:30:04 +0100

    Update to web application title

---
+++ src/Web/Dfe.CspdAlpha.Web/Dfe.CspdAlpha.Web.Application/Models/Common/Constants.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfe.CspdAlpha.Web.Application.Models.Common
+{
+    public class Constants
+    {
+        public const string APP_TITLE = "Review and amend school performance";
+    }
+}
