1d8cd165b1635519f61b05aeb0197182c9c840b7
James Rogers <james.rogers@spitewood.com>
2020-07-13 17:28:17 +0100

    Added Admin project and schools data import

---
+++ src/Admin/Dfe.CspdAlpha.Admin/NumberJsonConverter.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Dfe.CspdAlpha.Admin
+{
+    public class NumberJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+
+        public override bool CanRead
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            JToken t = JToken.FromObject(value);
+
+            if (t.Type != JTokenType.String)
+            {
+                t.WriteTo(writer);
+            }
+            else
+            {
+                if (int.TryParse(value.ToString(), out var intResult))
+                {
+                    t = JToken.FromObject(intResult);
+                }
+                else if (decimal.TryParse(value.ToString(), out var decResult))
+                {
+                    t = JToken.FromObject(decResult);
+                }
+
+                t.WriteTo(writer);
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException(
+                "Unnecessary because CanRead is false. The type will skip the converter.");
+        }
+    }
+}
---
+++ src/Admin/Dfe.CspdAlpha.Admin/SchoolsLoader.cs
@@ -0,0 +1,71 @@
+﻿using CsvHelper;
+using MoreLinq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfe.CspdAlpha.Admin
+{
+    public static class SchoolsLoader
+    {
+#if DEBUG
+        private const int MAX_PARALLELISM = 1;
+#else
+        private const int MAX_PARALLELISM = 10;
+#endif
+
+        private static volatile int _processedCount;
+
+        public static void Load(Action<string> log, string schoolsRefCsvFilePath, string schoolsPerfCsvFilePath)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            log($"{nameof(SchoolsLoader)} started");
+
+            ILookup<string, object> performanceLookup;
+            var decimalConverter = new NumberJsonConverter();
+
+            using (var reader = new StreamReader(schoolsPerfCsvFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<dynamic>();
+                performanceLookup = records.ToLookup(r => (string)r.DFESNumber);
+                log($"{schoolsPerfCsvFilePath} loaded");
+            }
+
+            using (var reader = new StreamReader(schoolsRefCsvFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<dynamic>();
+                log($"{schoolsRefCsvFilePath} loaded");
+
+                var batchNumber = 1;
+
+                foreach (IEnumerable<dynamic> batch in records.Batch(1000))
+                {
+                    Parallel.ForEach(batch, new ParallelOptions { MaxDegreeOfParallelism = MAX_PARALLELISM }, schoolRow =>
+                    {
+                        var perf = (IEnumerable<dynamic>)performanceLookup[schoolRow.DFESNumber];
+                        schoolRow.performance = perf.Select(r => new { r.Code, r.SetName, r.CodeValue });
+                        System.Threading.Interlocked.Increment(ref _processedCount);
+                    });
+
+                    File.WriteAllText(
+                        "schools_batch_" + batchNumber + ".json",
+                        JsonConvert.SerializeObject(batch, decimalConverter));
+
+                    log($"{batchNumber} batches processed");
+                    batchNumber++;
+                }
+            }
+
+            stopwatch.Stop();
+            log($"{nameof(SchoolsLoader)} finished in {stopwatch.Elapsed.Minutes}m {stopwatch.Elapsed.Seconds}s");
+        }
+    }
+}
