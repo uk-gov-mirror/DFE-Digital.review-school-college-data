8c280c9349f980ae0a3e96290bba27066d8f0f69
Andy Rose <andy.rose@olivejar.co.uk>
2020-09-21 13:58:33 +0100

    43468: changes to amendments page to support new prior attainment scores.

---
+++ src/Web/Dfe.CspdAlpha.Web/Dfe.CspdAlpha.Web.Application/Application/Helpers/RenderHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfe.CspdAlpha.Web.Application.Application.Helpers
+{
+    public class RenderHelper
+    {
+    }
+}
