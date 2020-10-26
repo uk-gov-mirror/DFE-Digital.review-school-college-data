74f34fab92f602b9979b34fde97e7329a12151b4
James Rogers <james.rogers@spitewood.com>
2020-10-26 12:27:03 +0000

    API - cancelling an amendment now also makes the record inactive

--- src/Api/Dfe.Rscd.Api/Dfe.Rscd.Api.Infrastructure/DynamicsCRM/Services/CrmAmendmentService.cs
+++ src/Api/Dfe.Rscd.Api/Dfe.Rscd.Api.Infrastructure/DynamicsCRM/Services/CrmAmendmentService.cs
@@ -433,7 +433,7 @@ namespace Dfe.Rscd.Api.Infrastructure.DynamicsCRM.Services
 
         public bool CancelAmendment(string id)
         {
-            var cols = new ColumnSet("rscd_outcome");
+            var cols = new ColumnSet("rscd_outcome", "statecode");
             var amendment = (rscd_Amendment)_organizationService.Retrieve(rscd_Amendment.EntityLogicalName, new Guid(id), cols);
 
             if (amendment == null || amendment.rscd_Outcome == rscd_Outcome.Cancelled)
@@ -442,6 +442,7 @@ namespace Dfe.Rscd.Api.Infrastructure.DynamicsCRM.Services
             }
 
             amendment.rscd_Outcome = rscd_Outcome.Cancelled;
+            amendment.StateCode = rscd_AmendmentState.Inactive;
 
             _organizationService.Update(amendment);
             return true;
