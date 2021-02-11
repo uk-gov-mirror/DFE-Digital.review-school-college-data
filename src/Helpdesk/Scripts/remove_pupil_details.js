1ac6482743f84c680c9b4ee9c92a03e0559624bb
jonbanner <jon@jonbanner.net>
2021-02-11 17:41:10 +0000

    Update remove_pupil_details.js

--- src/Helpdesk/Scripts/remove_pupil_details.js
+++ src/Helpdesk/Scripts/remove_pupil_details.js
@@ -1,18 +1,14 @@
 var Rscd = window.Rscd || {};
 (function () {
-// Code to run in the form OnLoad event
+ // Code to run in the form OnLoad event
   this.removePupilFormOnLoad = function (executionContext) {
     const formContext = executionContext.getFormContext();
- 
     const removePupilForm = formContext.ui.quickForms.get('Remove pupil details');
- 
     if (removePupilForm === undefined) {
       return;
     }
- 
     this.initRemovePupilForm(formContext, removePupilForm);
   }
- 
   this.initRemovePupilForm = function (formContext, removePupilForm) {
     if (!removePupilForm.isLoaded()) {
       setTimeout(this.initRemovePupilForm.bind(this), 10, formContext, removePupilForm);
@@ -23,40 +19,31 @@ console.log(reasonCode);
       switch (reasonCode) {
         // case 325: // Not at the end of 16 to 18 study
         //   break;
- 
         // case 326: // International student
         //   break;
- 
         // case 327: // Deceased
         //   break;
- 
         case 328: // Not on roll
          this.showFields(removePupilForm, ['rscd_allocationyeardescription', 'rscd_allocationyear_1description', 'rscd_allocationyear_2description']);
- 
           break;
- 
         case 330: // Other without evidence
           this.showFields(removePupilForm, ['rscd_allocationyeardescription']);
           break;
- 
         case 329: // Other with evidence
           this.showFields(removePupilForm, ['rscd_details']);
           break;
- 
-		case 8: // admitted from abroad English not 1st language
-			this.showFields(removePupilForm, ['rscd_language', 'rscd_countryoforigin' ,'rscd_dateofarrival']);
-			break;
- 
+     case 8: // admitted from abroad English not 1st language
+        this.showFields(removePupilForm, ['rscd_language', 'rscd_countryoforigin' ,'rscd_dateofarrival']);
+        break;
+    case 10: // Admitted following permanent exclusion from maintained school
+         this.showFields(removePupilForm, ['rscd_LAESTABofexcludedschool', 'rscd_Pupilexclusiondate']);
         // case 330: // Other without evidence
         //   break;
- 
           default:
           // no additional fields to show
       }
-     
     }
   }
- 
   this.showFields = function (removePupilForm, fieldsToShow,) {
     fieldsToShow.forEach(function(field){
       removePupilForm.getControl(field).setVisible(true);
