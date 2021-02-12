33768d7ad411561206c2b6791042aac36e569eed
jonbanner <jon@jonbanner.net>
2021-02-12 09:32:16 +0000

    Update remove_pupil_details.js

--- src/Helpdesk/Scripts/remove_pupil_details.js
+++ src/Helpdesk/Scripts/remove_pupil_details.js
@@ -37,6 +37,10 @@ console.log(reasonCode);
         break;
     case 10: // Admitted following permanent exclusion from maintained school
          this.showFields(removePupilForm, ['rscd_laestabofexcludedschool', 'rscd_pupilexclusiondate']);
+         break;
+    case 12: // Deceased
+          this.showFields(removePupilForm, ['rscd_dateoffroll']);
+          break;
         // case 330: // Other without evidence
         //   break;
           default:
