01f3dd3bdc37aa714a4da95f0eee506986e2d5f9
Marcel du Preez <marceldupreez@Marcels-MacBook-Pro.local>
2021-02-26 15:02:44 +0000

    Pulled all results fields through from DTO to Web

--- src/Api/Dfe.Rscd.Api/Dfe.Rscd.Api.Domain/Entities/Core/Result.cs
+++ src/Api/Dfe.Rscd.Api/Dfe.Rscd.Api.Domain/Entities/Core/Result.cs
@@ -1,19 +1,27 @@
-﻿namespace Dfe.Rscd.Api.Domain.Entities
+﻿using System;
+
+namespace Dfe.Rscd.Api.Domain.Entities
 {
     public class Result
     {
+        public int? ExamNumber { get; set; }
+        public int? ExamYear { get; set; }
+        public string SeasonCode { get; set; }
         public string SubjectCode { get; set; }
-        public string ExamYear { get; set; }
-        public string TestMark { get; set; }
-        public string ScaledScore { get; set; }
-        public string SeasonYear { get; set; }
         public string QualificationTypeCode { get; set; }
-        public string ExamDate { get; set; }
-        public string SyllabusCode { get; set; }
-        public string AwardingOrganisation { get; set; }
+        public int? AwardingBodyNumber { get; set; }
+        public string BoardSubjectNumber { get; set; }
+        public int? RIncl { get; set; }
         public string QAN { get; set; }
-        public string Subject { get; set; }
-        public string NCN { get; set; }
-        public string Grade { get; set; }
+        public int? MatchReg { get; set; }
+        public int? SubLevelCode { get; set; }
+        public string NationalCentreNumber { get; set; }
+        public double? FineGrade { get; set; }
+        public string TierCode { get; set; }
+        public string TestMark { get; set; }
+        public string GradeCode { get; set; }
+        public int? PortlandResultID { get; set; }
+        public DateTime ExamDate { get; set; }
+        public string ScaledScore { get; set; }
     }
 }
--- src/Web/Dfe.CspdAlpha.Web/Dfe.CspdAlpha.Web.Application/Models/ViewModels/Results/PriorAttainmentResultViewModel.cs
+++ src/Web/Dfe.CspdAlpha.Web/Dfe.CspdAlpha.Web.Application/Models/ViewModels/Results/PriorAttainmentResultViewModel.cs
@@ -8,13 +8,24 @@ namespace Dfe.Rscd.Web.Application.Models.ViewModels.Results
         public PupilViewModel PupilDetails { get; set; }
 
         public List<string> Ks2Subjects { get; set; }
-        public string Subject { get; set; }
-        public string ExamYear { get; set; }
-        public string TestMark { get; set; }
-        public string ScaledScore { get; set; }
-
+        public int? ExamNumber { get; set; }
+        public int? ExamYear { get; set; }
+        public string SeasonCode { get; set; }
         public string SubjectCode { get; set; }
-
         public string QualificationTypeCode { get; set; }
+        public int? AwardingBodyNumber { get; set; }
+        public string BoardSubjectNumber { get; set; }
+        public int? RIncl { get; set; }
+        public string QAN { get; set; }
+        public int? MatchReg { get; set; }
+        public int? SubLevelCode { get; set; }
+        public string NationalCentreNumber { get; set; }
+        public double? FineGrade { get; set; }
+        public string TierCode { get; set; }
+        public string TestMark { get; set; }
+        public string GradeCode { get; set; }
+        public int? PortlandResultID { get; set; }
+        public System.DateTime ExamDate { get; set; }
+        public string ScaledScore { get; set; }
     }
 }
